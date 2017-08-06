ce302eacf0b1bdac6a2b6d636c1f7795dd66f73e
Joshua Lee <jdl93@nau.edu>
2017-08-05 18:00:56 -1000

    test sql for adding into tables

---
+++ test.sql
@@ -0,0 +1,8 @@
+\c image_gallery
+
+INSERT INTO "Galleries" VALUES
+(default,
+'Josh Lee','http://4.bp.blogspot.com/-tR8OhrioILE/VN5XIn0OhCI/AAAAAAAAASs/hkjND6vm-Sw/s1600/Adam.jpg,
+This a test',
+NOW(),
+NOW());
