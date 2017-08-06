989e831746ff5132910af29da4c0854622cf6388
Joshua Lee <jdl93@nau.edu>
2017-08-05 20:12:13 -1000

    adding routes

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -2,10 +2,36 @@
 const express = require('express');
 const router = express.Router();
 const bp = require('body-parser');
+const db = require('../models');
+const Gallery = db.Gallery;
+
+router
+ .delete('/:id', (req, res) => {
+  console.log(`/gallery/${req.params.id} delete ID recieved`);
+  res.end();
+});
+
+router
+ .put('/:id', (req, res) => {
+  console.log(`/gallery/${req.params.id} PUT ID recieved`);
+  res.end();
+});
+
+router
+ .get('/:id/edit', (req, res) => {
+  console.log(`/gallery/${req.params.id}/edit GET ID and edit recieved`);
+  res.end();
+});
+
+router
+ .post('/', (req, res) => {
+  console.log('/gallery POST recieved');
+  res.end();
+});
 
 router
  .get('/new', (req, res) => {
-  console.log(`/gallery/new GET new recieved`);
+  console.log('/gallery/new GET new recieved');
   res.end();
 });
 
@@ -18,7 +44,14 @@ router
 router
  .get('/', (req, res) => {
   console.log('/gallery GET recieved');
-  res.end();
+  Gallery.findAll()
+  .then((gallery) => {
+    console.log('this is the gallery',gallery);
+    res.render('index', {gallery: gallery});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 });
 
 module.exports = router;
