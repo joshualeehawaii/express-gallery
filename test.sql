f89065c28560309161424db7707a8c9a01dba110
Joshua Lee <jdl93@nau.edu>
2017-08-10 21:44:49 -1000

    added a test file for the database

--- test.sql
+++ test.sql
@@ -7,3 +7,27 @@ INSERT INTO "Galleries" VALUES
 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ad et, dolorum cum officiis laboriosam',
 NOW(),
 NOW());
+
+INSERT INTO "Galleries" VALUES
+(default,
+'notcot.org',
+'http://www.notempire.com/images/uploads/topo-4.jpg',
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ad et, dolorum cum officiis laboriosam',
+NOW(),
+NOW());
+
+INSERT INTO "Galleries" VALUES
+(default,
+'notcot.org',
+'http://www.notempire.com/images/uploads/font-16.jpg',
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ad et, dolorum cum officiis laboriosam',
+NOW(),
+NOW());
+
+INSERT INTO "Galleries" VALUES
+(default,
+'notcot.org',
+'http://www.notempire.com/images/uploads/plantersq-2.jpg',
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ad et, dolorum cum officiis laboriosam',
+NOW(),
+NOW());
