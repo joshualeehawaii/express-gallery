2ccaca7e23c0052dd4584192e03c593a0e57c302
Joshua Lee <jdl93@nau.edu>
2017-08-10 21:44:26 -1000

    added override to the server

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const db = require('./models');
 const Gallery = db.Gallery;
 const galleryRoutes = require('./routes/galleryRoutes.js');
 const bp = require('body-parser');
+const methodOverride = require('method-override');
+
+//app level middleware to read the post data
+app.use(bp.urlencoded()); //this is to read the object being sent through from the post request
+
+//using method overirde to send put and delete requests
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 //handlebars
 require('handlebars');
@@ -17,8 +32,7 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-//app level middleware to read the post data
-app.use(bp.urlencoded()); //this is to read the object being sent through from the post request
+
 
 //this is mounting the routes to the server
 app.use('/gallery', galleryRoutes);
