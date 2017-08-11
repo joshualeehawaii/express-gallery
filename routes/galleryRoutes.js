221ff0922ba73f55ed1619f7a6cd7868adcf1f0b
Joshua Lee <jdl93@nau.edu>
2017-08-10 18:14:41 -1000

    changes

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -1,7 +1,6 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const router = express.Router();
-
 const db = require('../models');
 const Gallery = db.Gallery;
 
@@ -20,24 +19,25 @@ router
 router
  .get('/:id/edit', (req, res) => {
   console.log(`/gallery/${req.params.id}/edit GET ID and edit recieved`);
-  res.end();
+  res.render('edit');
 });
 
 router
- .post('/', (req, res) => {
-  console.log('/gallery POST recieved');
-  console.log('this is the request title =', req.body);
-  Gallery.create({
-    title: req.body.title,
+ .post('/',(req, res) => {
+   console.log('/gallery POST recieved');
+   console.log('this is the request title =', req.body);
+   return Gallery.create({
     author: req.body.author,
     link: req.body.link,
     description: req.body.description
-  }).then((data) => {
+  })
+  .then((data) => {
     console.log(data);
     console.log('new image post =', data);
     res.end();
-  }).catch((err) => {
-    console.log(err);
+  })
+  .catch((err) => {
+   console.log(err);
   });
 });
 
@@ -49,15 +49,15 @@ router
 
 router
  .get('/:id', (req, res) => {
-  console.log(`/gallery/${req.params.id} GET ID recieved`);
-  Gallery.findById(parseInt(req.params.id))
+   console.log(`/gallery/${req.params.id} GET ID recieved`);
+   Gallery.findById(parseInt(req.params.id))
   .then ((detail) => {
-   console.log('gallery id = ', detail);
-   var data = {
-    title: detail.tile,
-    author: detail.author,
-    link: detail.link,
-    description: detail.description
+    console.log('gallery id = ', detail);
+    var data = {
+     title: detail.tile,
+     author: detail.author,
+     link: detail.link,
+     description: detail.description
    };
    res.render('detail', data);
   })
@@ -68,14 +68,14 @@ router
 
 router
  .get('/', (req, res) => {
-  console.log('/gallery GET recieved');
-  Gallery.findAll()
+   console.log('/gallery GET recieved');
+   Gallery.findAll()
   .then((gallery) => {
    console.log('gallery = ', gallery);
    res.render('index', {gallery: gallery});
   })
   .catch((err) => {
-    console.log(err);
+   console.log(err);
   });
 });
 
