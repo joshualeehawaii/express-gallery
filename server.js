f141d9c15cad94a362c5eef7357c63410bbdb9fc
Joshua Lee <jdl93@nau.edu>
2017-08-19 09:19:10 -1000

    changes to the server

--- server.js
+++ server.js
@@ -3,14 +3,45 @@ const express = require('express');
 const app = express();
 const PORT = process.envPORT || 3000;
 const db = require('./models');
-const Gallery = db.Gallery;
+const {Gallery, User} = db;
+//const Gallery = db.Gallery;
 const galleryRoutes = require('./routes/galleryRoutes.js');
 const bp = require('body-parser');
 const methodOverride = require('method-override');
 
+//passports
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log('checking username', username);
+    console.log('checking password', password);
+    User.findOne({
+       where : {
+          username : username
+        }
+      }).then((user) => {
+      console.log('This is the user', user);
+      if (user.password === password) {
+        console.log('username and password successful');
+        return done(null, user);
+      } else {
+       console.log('password was incorrect');
+       return done(null, false, { message: 'incorrect password' });
+      }}).catch((err) => {
+       console.log('username not found');
+       console.log(err);
+      return done(null, false, { message: 'incorrect username' });
+    });
+  }));
+
+
 //exposing the public folder
 app.use(express.static('public'));
-
 
 //app level middleware to read the post data
 app.use(bp.urlencoded()); //this is to read the object being sent through from the post request
