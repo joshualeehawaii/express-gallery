622ba8821c048e507946e56222bb46c7ecd1bd4b
Joshua Lee <jdl93@nau.edu>
2017-08-19 19:04:20 -1000

    changes to server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
-const PORT = process.envPORT || 3000;
+const PORT = process.env.PORT || 3000;
 const db = require('./models');
 const {Gallery, User} = db;
 const galleryRoutes = require('./routes/galleryRoutes.js');
